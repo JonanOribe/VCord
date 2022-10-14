d3c2fdf36fa944a8fe5a44e02f5629e7fa4bda72
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-10-14 12:16:06 +0200

    Topics

--- vcord/base/views.py
+++ vcord/base/views.py
@@ -65,14 +65,16 @@ def home(request):
         )
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     context = {'rooms':rooms,
     'topics':topics,
-    'room_count':room_count}
+    'room_count':room_count,
+    'room_messages':room_messages}
     return render(request,'home.html',context)
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method=='POST':
         message = Message.objects.create(
@@ -80,6 +82,7 @@ def room(request,pk):
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
     context = {'room': room,'room_messages':room_messages,
     'participants':participants}
@@ -120,3 +123,13 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     return render(request,'delete.html',{'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!')
+    if request.method =='POST':
+        message.delete()
+        return redirect('home')
+    return render(request,'delete.html',{'obj':message})
